ab9e8c1e4b99561ce642b19efe03005871304552
TheSecretLife <akpn.qg@gmail.com>
2017-10-22 23:55:36 +0700

    Add files via upload

---
+++ tuple/ImmutablePair.java
@@ -0,0 +1,122 @@
+package org.apache.commons.lang3.tuple;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public final class ImmutablePair<L, R>
+  extends Pair<L, R>
+{
+  private static final ImmutablePair NULL = of(null, null);
+  
+
+  private static final long serialVersionUID = 4954918890077093841L;
+  
+
+  public final L left;
+  
+
+  public final R right;
+  
+
+
+  public static <L, R> ImmutablePair<L, R> nullPair()
+  {
+    return NULL;
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static <L, R> ImmutablePair<L, R> of(L left, R right)
+  {
+    return new ImmutablePair(left, right);
+  }
+  
+
+
+
+
+
+
+  public ImmutablePair(L left, R right)
+  {
+    this.left = left;
+    this.right = right;
+  }
+  
+
+
+
+
+  public L getLeft()
+  {
+    return left;
+  }
+  
+
+
+
+  public R getRight()
+  {
+    return right;
+  }
+  
+
+
+
+
+
+
+
+
+
+  public R setValue(R value)
+  {
+    throw new UnsupportedOperationException();
+  }
+}
